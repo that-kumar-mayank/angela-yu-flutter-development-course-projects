e2d9c7a3939f3e0229be56fefc1e2ed4195c3446
Kumar Mayank <kumarmayank0369@gmail.com>
2026-08-01 22:57:20 +0530

    Add Project 02: I Am Rich

---
+++ 02_i_am_rich/lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        backgroundColor: Colors.blueGrey,
+        appBar: AppBar(
+          title: Text('I Am Rich'),
+          centerTitle: true,
+          backgroundColor: Colors.blueGrey[900],
+        ),
+        body: Center(child: Image.asset('images/diamond.png')),
+      ),
+    ),
+  );
+}
